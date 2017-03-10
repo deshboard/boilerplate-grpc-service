21e8033d36300ed437d389f29accca7106b9a11f
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-03-10 14:16:33 +0100

    Remove extra space

--- proto.mk
+++ proto.mk
@@ -4,7 +4,7 @@ PROTO_PATH = vendor/github.com/deshboard/boilerplate-proto/proto
 
 proto: ## Generate code from protocol buffer
 	@mkdir -p proto
-	protowrap -I ${PROTO_PATH} ${PROTO_PATH}/boilerplate/boilerplate.proto ${PROTO_PATH}/boilerplate2/boilerplate2.proto  --go_out=plugins=grpc:proto
+	protowrap -I ${PROTO_PATH} ${PROTO_PATH}/boilerplate/boilerplate.proto ${PROTO_PATH}/boilerplate2/boilerplate2.proto --go_out=plugins=grpc:proto
 
 envcheck::
 	$(call executable_check,protoc,protoc)
