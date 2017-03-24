f8f351f54319807e89bbd4769c6097ede17af4cf
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-03-24 23:30:30 +0100

    Remove the need for protowrap

--- proto.mk
+++ proto.mk
@@ -4,8 +4,8 @@ PROTO_PATH = vendor/github.com/deshboard/boilerplate-proto/proto
 
 proto: ## Generate code from protocol buffer
 	@mkdir -p proto
-	protowrap -I ${PROTO_PATH} ${PROTO_PATH}/boilerplate/boilerplate.proto ${PROTO_PATH}/boilerplate2/boilerplate2.proto --go_out=plugins=grpc:proto
+	protoc -I ${PROTO_PATH} ${PROTO_PATH}/boilerplate/boilerplate.proto --go_out=plugins=grpc:proto
+	protoc -I ${PROTO_PATH} ${PROTO_PATH}/boilerplate2/boilerplate2.proto --go_out=plugins=grpc:proto
 
 envcheck::
 	$(call executable_check,protoc,protoc)
-	$(call executable_check,protowrap,protowrap)
