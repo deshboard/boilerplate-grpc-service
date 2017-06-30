a2f811c5df003db15735402bab58ef9c1c6c2204
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-06-30 02:30:52 +0200

    Add error handler to service

--- app/service.go
+++ app/service.go
@@ -1,15 +1,17 @@
 package app
 
-import "github.com/go-kit/kit/log"
+import (
+	"github.com/go-kit/kit/log"
+	"github.com/goph/emperror"
+)
 
 // Service implements the RPC server
 type Service struct {
-	logger log.Logger
+	logger       log.Logger
+	errorHandler emperror.Handler
 }
 
 // NewService creates a new service object
-func NewService(logger log.Logger) *Service {
-	return &Service{
-		logger: logger,
-	}
+func NewService(logger log.Logger, errorHandler emperror.Handler) *Service {
+	return &Service{logger, errorHandler}
 }
