80d58646a5bbee0905fb1e650f84ccd54fc5c56a
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-08-23 08:52:45 +0200

    Fix interface

--- app/service.go
+++ app/service.go
@@ -3,12 +3,13 @@ package app
 import (
 	"github.com/go-kit/kit/log"
 	"github.com/goph/emperror"
+	"github.com/goph/stdlib/errors"
 )
 
 // Service implements the RPC server.
 type Service struct {
 	Logger       log.Logger
-	ErrorHandler emperror.Handler
+	ErrorHandler errors.Handler
 }
 
 // NewService creates a new service object.
