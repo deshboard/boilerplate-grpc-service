f53f0dd5461293298a41d8317c1f05160e233b07
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-09-13 19:39:46 +0200

    Rename appCtx to app

--- cmd/grpc.go
+++ cmd/grpc.go
@@ -11,22 +11,22 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
-func createGrpcServer(appCtx *application) *grpc.Server {
+func createGrpcServer(app *application) *grpc.Server {
 	// TODO: separate log levels
 	grpclog.SetLoggerV2(grpclog.NewLoggerV2(
-		log.NewStdlibAdapter(level.Info(appCtx.logger)),
-		log.NewStdlibAdapter(level.Warn(appCtx.logger)),
-		log.NewStdlibAdapter(level.Error(appCtx.logger)),
+		log.NewStdlibAdapter(level.Info(app.logger)),
+		log.NewStdlibAdapter(level.Warn(app.logger)),
+		log.NewStdlibAdapter(level.Error(app.logger)),
 	))
 
 	server := grpc.NewServer(
 		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
-			grpc_opentracing.StreamServerInterceptor(grpc_opentracing.WithTracer(appCtx.tracer)),
+			grpc_opentracing.StreamServerInterceptor(grpc_opentracing.WithTracer(app.tracer)),
 			grpc_prometheus.StreamServerInterceptor,
 			grpc_recovery.StreamServerInterceptor(),
 		)),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-			grpc_opentracing.UnaryServerInterceptor(grpc_opentracing.WithTracer(appCtx.tracer)),
+			grpc_opentracing.UnaryServerInterceptor(grpc_opentracing.WithTracer(app.tracer)),
 			grpc_prometheus.UnaryServerInterceptor,
 			grpc_recovery.UnaryServerInterceptor(),
 		)),
