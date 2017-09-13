70c4625d28dd8903f27d6fa99dbda005ee256cac
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-09-13 19:39:28 +0200

    Improve grpc logging

--- cmd/grpc.go
+++ cmd/grpc.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 )
 
-func init() {
-	// TODO: Set global gRPC logger
-	// grpclog.SetLogger()
-}
-
 func createGrpcServer(appCtx *application) *grpc.Server {
+	// TODO: separate log levels
+	grpclog.SetLoggerV2(grpclog.NewLoggerV2(
+		log.NewStdlibAdapter(level.Info(appCtx.logger)),
+		log.NewStdlibAdapter(level.Warn(appCtx.logger)),
+		log.NewStdlibAdapter(level.Error(appCtx.logger)),
+	))
+
 	server := grpc.NewServer(
 		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
 			grpc_opentracing.StreamServerInterceptor(grpc_opentracing.WithTracer(appCtx.tracer)),
